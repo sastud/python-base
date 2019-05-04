2fd59d01c4bad048e0387cf52f10d1e971406684
sastud <svinarev.sa@yandex.ru>
2019-05-04 22:44:57 +0300

    1

--- lesson5/lib/lesson5_hw_easy_lib.py
+++ lesson5/lib/lesson5_hw_easy_lib.py
@@ -2,24 +2,29 @@
 import os
 
 
-for i in range(1, 10):
-    try:
-        os.mkdir(os.path.join(os.getcwd(), f'dir_{i}'))
-        print(f'Директория dir_{i} создана')
-    except FileExistsError:
-        print(f'Директория dir_{i} уже существует')
+def my_cd(path):
+    os.chdir(path)
+    print(f'Текущая директория: {path}')
 
-print(input('\nДалее созданные директории будут удалены. Нажмите ENTER для продолжения...'))
 
-for i in range(1, 10):
+def my_ls():
+    print('Содержимое текущей директории: ')
+    list_cur_dir = os.listdir(os.path.join(os.getcwd()))
+    for i in range(len(list_cur_dir)):
+        print(list_cur_dir[i])
+
+
+def my_rm(dir_name):
     try:
-        os.rmdir(os.path.join(os.getcwd(), f'dir_{i}'))
-        print(f'Директория dir_{i} удалена')
+        os.rmdir(os.path.join(os.getcwd(), dir_name))
+        print(f'Директория {dir_name} удалена')
     except FileNotFoundError:
-        print(f'Директория dir_{i} не найдена')
+        print(f'Директория {dir_name} не найдена')
 
 
-print('Содержимое текущей директории: ')
-list_cur_dir = os.listdir(os.path.join(os.getcwd()))
-for i in range(len(list_cur_dir)):
-    print(list_cur_dir[i])
+def my_mkdir(dir_name):
+    try:
+        os.mkdir(os.path.join(os.getcwd(), dir_name))
+        print(f'Директория {dir_name} создана')
+    except FileExistsError:
+        print(f'Директория {dir_name} уже существует')
